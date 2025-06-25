36829629f553dc00d61fd20d5cf5c1b3f64bad17
Emily Nguyen <emilyn1806@gmail.com>
2025-06-25 12:06:21 -0800

    add a login api

--- server/index.js
+++ server/index.js
@@ -18,6 +18,20 @@ app.get('/db-test', async (req, res) => {
   res.json({ ok: true, userCount });
 });
 
+app.post('/api/users/login', async (req, res) => {
+  const { username } = req.body;
+  if (!username) {
+    return res.status(400).json({ error: 'username required' });
+  }
+  
+  let user = await prisma.user.findUnique({ where: { username } });
+  if (!user) {
+    user = await prisma.user.create({ data: { username } });
+  }
+  
+  res.json({ user: { id: user.id, username: user.username } });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
